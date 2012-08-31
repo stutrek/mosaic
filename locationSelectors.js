d47da299f29fb80c3164a484d159ea2cb0d2c755
Stu Kabakoff <sakabako@gmail.com>
2012-08-31 17:35:55 -0400

    weighted location selector tries to waste as little area as possible may or may not be worthwhile.

--- locationSelectors.js
+++ locationSelectors.js
@@ -1,16 +1,37 @@
 define('locationSelectors', function(require, exports, module) {
 	
 	var selectors = {};
-	exports.both = function both( locations ) {
-		var bestLocation = locations[0];
-		for( var i = locations.length-1; i > -1; i -= 1 ) {
-			var location = locations[i];
-			if (location.top <= bestLocation.top && location.wastedArea <= bestLocation.wastedArea) {
-				bestLocation = location;
+	
+	function getMinimum( array, prop ) {
+		var min = Infinity;
+		array.forEach(function(item) {
+			min = Math.min(item[prop], min);
+		});
+		return min;
+	}
+	
+	selectors.weighted = function( jitter ) {
+		var wastedAreaJitter = jitter * jitter * 0.25;
+		var maxDistanceFromMinTop = jitter * 5;
+		var topSelector = selectors.top(jitter);
+		return function wastedArea( locations ) {
+			
+			// filter out locations that waste too much space.
+			var currentWastedAreaJitter = wastedAreaJitter * locations[0].columns.length * 0.75;
+			var maxAllowedWastedArea = getMinimum(locations, 'wastedArea') + currentWastedAreaJitter;
+			var maxAllowedTop = getMinimum(locations, 'top') + maxDistanceFromMinTop;
+			console.log(maxDistanceFromMinTop);
+			var filteredLocations = locations.filter(function( location ) {
+				return ( location.wastedArea < maxAllowedWastedArea && location.top < maxAllowedTop );
+			});
+			if (filteredLocations.length === 0) {
+				filteredLocations = locations;
 			}
+			console.log(currentWastedAreaJitter, locations.length, filteredLocations.length, locations.map(function(l){return l.wastedArea}).toString());
+			
+			return topSelector(filteredLocations);
 		}
-		return bestLocation;
-	};
+	}
 	
 	selectors.top = function( jitter ) {
 		jitter = jitter || 0;
